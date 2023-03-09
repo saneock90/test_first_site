cc30eba22b8c55fb81bc3b0da6c1951c28d608ac
Peancovschii Alexandr <a.peancovschii@gmail.com>
2023-03-09 20:26:04 +0200

    commit2

--- news/forms.py
+++ news/forms.py
@@ -15,6 +15,7 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        # a;sldkfjalkghalkdfgh/
 
 
 
