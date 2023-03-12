50f68c4cfbef15bcdd1fa781f708a3f590ba481a
Peancovschii Alexandr <a.peancovschii@gmail.com>
2023-03-12 11:41:20 +0200

    user login/logout ready to use

--- news/views.py
+++ news/views.py
@@ -4,27 +4,41 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.contrib import messages
+from django.contrib.auth import login, logout
 
 
 from .models import News, Category
-from .forms import NewsForm, UserRegisterForm
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
 from .utils import MyMixin
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Пользователь зареган')
-            return redirect('login')
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Мои поздравления! Ты зареган!')
+            return redirect('home')
         else:
             messages.error(request, 'Ошибка блягь')
     else:
         form = UserRegisterForm()
     return render(request, 'news/register.html', {"form": form})
 
-def login(request):
-    return render(request, 'news/login.html')
+def user_login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {"form": form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 def test(request):
     objects = ['john1','paul2','george3','ringo4','john5','paul6','george7','ringo8','john9','paul10','george11']
